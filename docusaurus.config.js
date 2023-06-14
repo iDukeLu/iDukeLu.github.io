622573466682266086a8fe59c039088c3c3f39fa
luhy01 <luhy01@mingyuanyun.com>
2023-06-14 18:34:23 +0800

    修改 Navbar 名称

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,9 +87,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '一心净土',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Plane of Euthymia',
           src: 'img/logo.svg',
         },
         items: [
