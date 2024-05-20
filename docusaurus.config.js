ad1614ffb9ef10a4180829c2fb1dd37a4646c68f
luhongyu <luhy01@mingyuanyun.com>
2024-05-20 14:17:32 +0800

    Updata config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,7 @@ const config = {
         docs: {
           sidebarPath: './sidebars.js',
           breadcrumbs: true, 
+          sidebarCollapsed: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
